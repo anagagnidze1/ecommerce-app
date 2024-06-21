8636021e7b4897f9cd5171d3d6b6b29a2dbfb3bd
Luka Umfriani <105096407+lacertaee@users.noreply.github.com>
2024-06-21 03:22:50 -0700

    Revert "Release/basket about us"

--- javascript/cart.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.cart').addEventListener('click', () => {
-        window.location.href = '../src/catalog.html'
-    })
-    console.log('Rendering cart:', products)
-})
